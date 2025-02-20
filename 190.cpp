d50ba03e0a5cb56477c5c03a9afd77e815fd410e
arunyadav090 <arunkumar9799a@gmail.com>
2025-02-19 20:49:03 -0800

    Create 190.cpp

---
+++ 190.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        return ans;
+    }
+};
+
+*test case*
+
+Case 1
+Case 2
+Input
+n =
+00000010100101000001111010011100
+Output
+   964176192 (00111001011110000010100101000000)
+Expected
+   964176192 (00111001011110000010100101000000)
